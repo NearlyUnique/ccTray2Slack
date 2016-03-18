d884d71b61c32d31b0259d8b0223aa8be48d25b0
Christer Eriksson <christer.j.eriksson@gmail.com>
2016-03-18 20:22:23 +0100

    Specify username/password from docker-compose file

--- docker/run_ccTray2Slack.sh
+++ docker/run_ccTray2Slack.sh
@@ -2,4 +2,4 @@
 rm -f ccTray2Slack
 wget https://github.com/christer79/ccTray2Slack/releases/download/${VERSION}/ccTray2Slack -nv
 chmod u+x ccTray2Slack
-./ccTray2Slack -config /root/config/config.json -username christer.eriksson -password p4rqBE26
+./ccTray2Slack -config /root/config/config.json -username ${USERNAME} -password ${PASSWORD}
