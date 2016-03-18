af0feedca503dda4d518de98ffd85a029f263be6
Christer Eriksson <christer.j.eriksson@gmail.com>
2016-03-18 20:05:27 +0100

    Possible to specify version
    Made a script for wget and start so that a version canbe specified in the docker-compose-file

---
+++ docker/run_ccTray2Slack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f ccTray2Slack
+wget https://github.com/christer79/ccTray2Slack/releases/download/${VERSION}/ccTray2Slack -nv
+chmod u+x ccTray2Slack
+./ccTray2Slack -config /root/config/config.json -username ***REMOVED*** -password ***REMOVED***
