bad2b9e2192b8accef06dab9c4249f6a85ac198f
Christer Eriksson <christer.j.eriksson@gmail.com>
2016-03-27 16:49:50 +0200

    Tests for Listrojects

--- ccTray_test.go
+++ ccTray_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -8,7 +9,7 @@ import (
 )
 
 var (
-	ccXml = []string{
+	ccXML = []string{
 		`<Projects>
 	<Project name="Project 1" activity="Sleeping" lastBuildStatus="Success" lastBuildLabel="1.2.3" lastBuildTime="2009-07-27T14:17:19" webUrl="http://localhost:8153/cruise/v1"/>
 	<Project name="Project 2" activity="Building" lastBuildStatus="Success" lastBuildLabel="1.2.6" lastBuildTime="2009-07-27T14:17:19" webUrl="http://localhost:8153/cruise/v2"/>
@@ -39,11 +40,29 @@ var (
 		"name=Project 3, activity=Sleeping, status=Failed, label=1.2.7, time=2009-07-27 14:17:19 +0000 UTC, url=http://localhost:8153/cruise/v3, transition=Broken"}
 )
 
+func TestListProjects(t *testing.T) {
+	ccXMLCopy := ccXML
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var popped string
+		w.Header().Set("Content-Type", "application/json")
+		popped, ccXMLCopy = ccXMLCopy[len(ccXMLCopy)-1], ccXMLCopy[:len(ccXMLCopy)-1]
+		fmt.Fprintln(w, popped)
+	}))
+
+	defer ts.Close()
+	sut := CreateCcTray(ts.URL)
+	buf := &bytes.Buffer{}
+	out = buf
+	sut.ListProjects()
+	expect := "Project 1\nProject 2\nProject 3\n"
+	equalString(t, buf.String(), expect)
+}
+
 func TestIt(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var popped string
 		w.Header().Set("Content-Type", "application/json")
-		popped, ccXml = ccXml[len(ccXml)-1], ccXml[:len(ccXml)-1]
+		popped, ccXML = ccXML[len(ccXML)-1], ccXML[:len(ccXML)-1]
 		fmt.Fprintln(w, popped)
 	}))
 	count := 0
