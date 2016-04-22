d0fbbd3ec1af54e5b8d469c33a621062d6593816
Christer Eriksson <christer.j.eriksson@gmail.com>
2016-04-22 20:20:49 +0200

    Possible to log to file

--- docker/run_ccTray2Slack.sh
+++ docker/run_ccTray2Slack.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-rm -f ccTray2Slack
-wget https://github.com/christer79/ccTray2Slack/releases/download/${VERSION}/ccTray2Slack -nv
+#!/bin/bash -x
+[ -f ccTray2Slack ] || wget https://github.com/christer79/ccTray2Slack/releases/download/${VERSION}/ccTray2Slack -nv
 chmod u+x ccTray2Slack
-./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start
+echo ./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start -log ${LOGFILE}
+./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start -log ${LOGFILE} &
+tail -F ${LOGFILE}
