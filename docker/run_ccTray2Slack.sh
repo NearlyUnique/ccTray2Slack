9c44303fc0c31614ca59e24a6a3b19f857059141
Christer Eriksson <christer.j.eriksson@gmail.com>
2016-04-25 17:41:45 +0200

    Adding --port when starting docker

--- docker/run_ccTray2Slack.sh
+++ docker/run_ccTray2Slack.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 [ -f ccTray2Slack ] || wget https://github.com/christer79/ccTray2Slack/releases/download/${VERSION}/ccTray2Slack -nv
 chmod u+x ccTray2Slack
-echo ./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start -log ${LOGFILE}
-./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start -log ${LOGFILE} &
+./ccTray2Slack -config /root/config/ -username ${USERNAME} -password ${PASSWORD} start -log ${LOGFILE} --port ${PORT} &
 tail -F ${LOGFILE}
