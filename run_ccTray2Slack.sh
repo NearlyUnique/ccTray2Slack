46a86034fda6420190138c45d57ffbd2f4632953
Christer Eriksson <christer.eriksson@jeppesen.com>
2016-04-26 21:24:59 +0200

    Adding local ip in consul registration

--- run_ccTray2Slack.sh
+++ run_ccTray2Slack.sh
@@ -2,7 +2,7 @@
 
 CONSUL_ARGS=""
 if [ x$CONSUL_URL != x"" ] ; then
-  CONSUL_ARGS="--consul-url $CONSUL_URL --consul-address 10.10.01.01"
+  CONSUL_ARGS="--consul-url $CONSUL_URL --consul-address $(hostname --ip-address)"
 fi
 ./ccTray2Slack --log ${LOGFILE} --config /root/config/ --username ${USERNAME} --password ${PASSWORD} start --port ${PORT} ${CONSUL_ARGS} &
 tail -F ${LOGFILE}
